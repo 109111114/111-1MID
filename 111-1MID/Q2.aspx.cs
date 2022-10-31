6d829e60a684adae7f7a98cf4dfd12e58975fb41
俊毅 林 <109111114@mail.aeust.edu.tw>
2022-10-31 10:20:44 +0800

    Q2修改1

--- 111-1MID/Q2.aspx.cs
+++ 111-1MID/Q2.aspx.cs
@@ -15,7 +15,37 @@ namespace _111_1MID {
         };
 
         protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack)
+            {
+                for (int i_ct = 0; i_ct < s_City.Length; i_ct++)
+                {
+                    ListItem L_i = new ListItem();
+                    L_i.Text = s_City[i_ct];
+                    L_i.Value = s_City[i_ct];
 
+                    dpl_City.Items.Add(L_i);
+                }
+                mt_GenSecondList();
+            }
+        }
+
+        protected void mt_GenSecondList()
+        {
+            int i_ind = dpl_City.SelectedIndex;
+            dpl_Area.Items.Clear();
+            for (int i_ct = 0; i_ct < s_Area.GetLength(1); i_ct++)
+            {
+                ListItem L_i = new ListItem();
+                L_i.Text = s_Area[i_ind, i_ct];
+                L_i.Value = s_Area[i_ind, i_ct];
+
+                dpl_Area.Items.Add(L_i);
+            }
+        }
+
+        protected void dpl_City_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mt_GenSecondList();
         }
     }
 }
--- 111-1MID/Q2_Sub.aspx.cs
+++ 111-1MID/Q2_Sub.aspx.cs
@@ -8,7 +8,12 @@ using System.Web.UI.WebControls;
 namespace _111_1MID {
     public partial class Q2_Sub : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            string[] s_id = new string[] { "tb_Num", "rbl_Phone", "txt_Phone", "dpl_City", "dpl_Area" };
+            string[] s_type = new string[] { "保單號碼:", "通訊種類:", "通訊號碼:", "所在城市:", "所在區域:" };
+            for (int i_ct = 0; i_ct < s_id.Length; i_ct++)
+            {
+                lb_Msg.Text += s_type[i_ct] + " " + Request.Form.Get(s_id[i_ct]) + "<br />";
+            }
         }
     }
 }
