1a912ead8fba5b8226f16b9219011d741f0689f9
俊毅 林 <109111114@mail.aeust.edu.tw>
2022-10-31 10:08:26 +0800

    Q1完成1

--- 111-1MID/Q1.aspx.cs
+++ 111-1MID/Q1.aspx.cs
@@ -12,5 +12,32 @@ namespace _111_1MID {
         protected void Page_Load(object sender, EventArgs e) {
 
         }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            pl_Msg.Visible = true;
+            lb_Msg.Text = lb_Type.Text + "<br />" + tb_Account.Text + "先生/小姐，以完成掛號。" + "<br />";
+            if (tb_Phone.Text != "")
+            {
+                lb_Msg.Text += "連絡電話為" + tb_Phone.Text + "<br />";
+            }
+        }
+        protected void tb_Account_TextChanged(object sender, EventArgs e)
+        {
+            for (int i_ct = 0; i_ct < s_IdSet.Length; i_ct++)
+            {
+                if (tb_Account.Text == s_IdSet[i_ct])
+                {
+                    lb_Type.Text = "複診";
+                    break;
+                }
+                else
+                {
+                    lb_Type.Text = "初診";
+                }
+            }
+            btn_Submit.Visible = ((tb_Account.Text != "") ? true : false);
+            pl_Msg.Visible = false;
+        }
     }
 }
